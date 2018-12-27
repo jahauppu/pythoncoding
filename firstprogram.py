e87e3d16c5d0f6a12223bffcc3237d82e51c2152
jahauppu <jahauppu@in.ibm.com>
2018-12-27 13:18:16 +0530

    sftcommit

---
+++ firstprogram.py
@@ -0,0 +1,42 @@
+Name="Michael Jackson"
+print(Name)
+print(Name.find('el'))
+
+
+Numbers="0123456"
+print(Numbers[::2])
+print(Numbers.find('1'))
+
+print(3+2*2)
+
+Nam='Lizz'
+print(Nam[0:2])
+
+var='01234567'
+print(var[::2])
+print(var[::3])
+
+fname = "Michael Jackson"
+print(fname[0])
+#print(fname[16]) #'''Index error : String index out of range '''
+print(fname[0:5])
+print(fname[4:9:2])
+
+print(r"Michael Jackson \n is the best")
+lname=fname.replace('Michael', 'Mallika')
+print(lname)
+print(fname.find('al'))
+
+A="1"
+print(id(fname))
+#end of file
+
+
+
+
+
+
+
+
+
+
